d41d0b812a1ea3b60262f63f7122ac5e9fb8f8c3
David Osorio <davidosoriorodriguez@gmail.com>
2022-06-17 14:07:44 +0200

    feat: change app to render appbar

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,31 +1,29 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HOME_PATH, TICKETS_PATH } from "../../constants/routes";
+import { Web3Provider } from "../../context/Web3";
 
 import Home from "../../routes/Home";
 import Tickets from "../../routes/Tickets";
 import AppBar from "../AppBar";
 
-import Footer from "../Footer"; 
-
 import useStyles from "./App.style";
 
-function App() {
+const App = () => {
   const classes = useStyles();
   return (
-    <BrowserRouter>
-      <div className={classes.root}>
-        <AppBar />
-        <Routes>
-          <Route exact path={HOME_PATH} element={<Home />} />
-          <Route exact path={TICKETS_PATH} element={<Tickets />} />
-        </Routes>
-      </div>
-      {/* <div className={classes.footer}>
-        <Footer />
-      </div> */}
-    </BrowserRouter>
+    <Web3Provider>
+      <BrowserRouter>
+        <div className={classes.root}>
+          <AppBar />
+          <Routes>
+            <Route exact path={HOME_PATH} element={<Home />} />
+            <Route exact path={TICKETS_PATH} element={<Tickets />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Web3Provider>
   );
-}
+};
 
 export default App;
