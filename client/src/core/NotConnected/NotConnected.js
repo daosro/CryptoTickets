24034c004166eaeb5267b5a401a18b1307cbb0d1
David Osorio <davidosoriorodriguez@gmail.com>
2022-06-22 19:53:06 +0200

    feat: add NoConnected component

--- client/src/core/NotConnected/NotConnected.js
+++ client/src/core/NotConnected/NotConnected.js
@@ -1,10 +1,39 @@
 import React from "react";
+import ConnectButton from "../ConnectButton";
 
 import useStyles from "./NotConnected.style";
 
 const NotConnected = () => {
   const classes = useStyles();
-  return <div className={classes.root}>TODO: Connect to metamask please</div>;
+  return (
+    <div className={classes.root}>
+      <div className={classes.title}>CONNECT WALLET</div>
+      <div className={classes.connectText}>
+        Connect with your available waller or create new wallet to join our
+        marketplace
+      </div>
+      <ConnectButton />
+      <div className={classes.installMetamask}>
+        Don't have Metamask installed yet?
+        <a
+          href="https://metamask.io/download/"
+          target="_blank"
+          rel="noreferrer"
+          className={classes.metamaskLink}
+        >
+          <img
+            className={classes.logo}
+            src={process.env.PUBLIC_URL + "/assets/images/logo_metamask.svg"}
+            alt="Champions 14"
+          />
+        </a>
+      </div>
+      <div className={classes.legal}>
+        We do not own your private keys and cannot acces your funds without your
+        confirmation
+      </div>
+    </div>
+  );
 };
 
 export default NotConnected;
--- client/src/core/NotConnected/NotConnected.style.js
+++ client/src/core/NotConnected/NotConnected.style.js
@@ -2,7 +2,39 @@ import { createUseStyles } from "react-jss";
 
 export default createUseStyles(
   {
-    root: {},
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      justifySelf: "center",
+      alignItems: "center",
+    },
+    title: {
+      margin: "10px",
+      fontSize: "20px",
+      fontWeight: "bold",
+    },
+    connectText: {
+      margin: "20px",
+      fontSize: "15px",
+    },
+    installMetamask: {
+      margin: "20px 0",
+    },
+    metamaskLink: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "10px",
+      "& img": {
+        margin: "20px 0 0",
+        maxWidth: "10rem",
+      },
+    },
+    legal: {
+      fontSize: "10px",
+    },
   },
   { name: "NotConnected" }
 );
