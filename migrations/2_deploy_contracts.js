3b51f345a540d46cec07347c1c600d3a63d4578f
David Osorio <davidosoriorodriguez@gmail.com>
2022-06-28 22:32:40 +0200

    change truffle script to allow comunication between contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,13 @@
 var STRMMembership = artifacts.require("./STRMMembership.sol");
-var STRMMatchTickets = artifacts.require("./Match.sol");
+var STRMMatchTickets = artifacts.require("./STRMMatchTickets.sol");
 
-module.exports = function (deployer) {
-  deployer.deploy(STRMMembership);
-  deployer.deploy(STRMMatchTickets);
+module.exports = async function (deployer) {
+  await deployer.deploy(STRMMatchTickets);
+
+  await deployer.deploy(STRMMembership, STRMMatchTickets.address);
+
+  var strmMatchTickets = await STRMMatchTickets.deployed();
+
+  await strmMatchTickets.grantAdminRol(STRMMembership.address);
 };
 4;
