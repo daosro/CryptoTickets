e15a0fb38643d7d1556e7aa45eaad7466299297e
David Osorio <davidosoriorodriguez@gmail.com>
2022-06-30 00:22:32 +0200

    feat: add nfts to the view

--- client/src/routes/Tickets/Tickets.js
+++ client/src/routes/Tickets/Tickets.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { USER_ZONE_SUBSCRIBER_PATH } from "../../constants/routes";
 import { Web3Context } from "../../context/Web3";
-import Card from "../../core/Card";
+import Card, { CardContainer } from "../../core/Card";
 import Button from "../../core/Button";
 import NonFungibleToken from "../../core/NonFungibleToken";
 
@@ -14,6 +14,7 @@ const Tickets = () => {
   const classes = useStyles();
   const { accounts, contracts } = useContext(Web3Context);
   const [subscriberNFTMetadata, setSubscriberNFTMetadata] = useState(null);
+  const [ticketsNFTsMetadata, setTicketsNFTsMetadata] = useState([]);
 
   useEffect(() => {
     if (accounts?.[0] || contracts.lenght) {
@@ -38,6 +39,31 @@ const Tickets = () => {
         } else {
           setSubscriberNFTMetadata(null);
         }
+        const ticketsLength = await contracts.matchTickets.methods
+          .balanceOf(account)
+          .call();
+        if (ticketsLength > 0) {
+          const tokensMetadata = [];
+
+          for (let i = 0; i < ticketsLength; i++) {
+            const tokenId = await contracts.matchTickets.methods
+              .tokenOfOwnerByIndex(account, i)
+              .call();
+            try {
+              const tokenMetadataURI = await contracts.matchTickets.methods
+                .tokenURI(tokenId)
+                .call();
+
+              const metadata = await getNFTMetadataObject(tokenMetadataURI);
+              tokensMetadata.push({ ...metadata, tokenId: tokenId });
+            } catch (error) {
+              console.error(error);
+            }
+          }
+          setTicketsNFTsMetadata(tokensMetadata);
+        } else {
+          setTicketsNFTsMetadata([]);
+        }
       };
       getUserToken();
     }
@@ -46,20 +72,25 @@ const Tickets = () => {
   return (
     <div className={classes.root}>
       <h2>Mis entradas</h2>
-      {subscriberNFTMetadata ? (
-        <NonFungibleToken metadata={subscriberNFTMetadata} />
-      ) : (
-        <Card xs={12} sm={6} md={3} lg={4} header={"Aún no eres socio"}>
-          <div className={classes.cardContent}>
-            <div className={classes.cardText}>
-              No esperes más y hazte socio hoy mismo
+      <CardContainer>
+        {subscriberNFTMetadata ? (
+          <NonFungibleToken metadata={subscriberNFTMetadata} />
+        ) : (
+          <Card xs={12} sm={6} md={3} lg={4} header={"Aún no eres socio"}>
+            <div className={classes.cardContent}>
+              <div className={classes.cardText}>
+                No esperes más y hazte socio hoy mismo
+              </div>
+              <Link to={USER_ZONE_SUBSCRIBER_PATH}>
+                <Button className={classes.carHeader}>Quiero ser socio</Button>
+              </Link>
             </div>
-            <Link to={USER_ZONE_SUBSCRIBER_PATH}>
-              <Button className={classes.carHeader}>Quiero ser socio</Button>
-            </Link>
-          </div>
-        </Card>
-      )}
+          </Card>
+        )}
+        {ticketsNFTsMetadata.map((metadata) => (
+          <NonFungibleToken metadata={metadata} />
+        ))}
+      </CardContainer>
     </div>
   );
 };
