8ab3207462438fa27ea6afcacd9b6f31c2b913c6
David Osorio <davidosoriorodriguez@gmail.com>
2022-07-06 23:51:48 +0200

    feat: add new home

--- client/src/routes/Home/Home.style.js
+++ client/src/routes/Home/Home.style.js
@@ -12,17 +12,52 @@ export default createUseStyles(
     groupStyles: {
       right: "1.8rem",
       bottom: "4rem",
-      position: "absolute",
+      position: "fixed",
       display: "flex",
       flexDirection: "column",
       justifyContent: "flex-end",
       alignItems: "flex-start",
+      backgroundColor: "#fff",
+      padding: "10px",
       "& span": {
         marginBottom: "10px",
       },
     },
     selectContainer: {
       minWidth: "300px",
+    },
+    content: {
+      fontFamily: "real-madrid-icons",
+      fontSize: "large",
+      textAlign: "justify",
+      margin: "2rem",
+    },
+    developers: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "row",
+      margin: "2rem 2rem 5rem 2rem",
+      "@media (max-width: 800px)": {
+        flexDirection: "column",
+      },
+    },
+    developerContainer: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      flexDirection: "column",
+      fontSize: "x-large",
+      fontWeight: 500,
+      margin: "2rem",
+      "& a": {
+        marginTop: "1rem",
+        color: "#0a66c2",
+        cursor: "pointer",
+      },
+      "& img": {
+        borderRadius: "100px",
+      },
     },
   },
   { name: "Home" }
